476dcf885e086adb400df38c76023e0c960e6d68
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-09-30 23:44:53 +0100

    auth.go

---
+++ auth/auth.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+    "errors"
+    "os"
+    "time"
+
+    "github.com/dgrijalva/jwt-go"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+type Claims struct {
+    Username string `json:"username"`
+    jwt.StandardClaims
+}
+
+func GenerateToken(username string) (string, error) {
+    expirationTime := time.Now().Add(24 * time.Hour)
+    claims := &Claims{
+        Username: username,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: expirationTime.Unix(),
+        },
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    tokenString, err := token.SignedString(jwtSecret)
+    if err != nil {
+        return "", err
+    }
+
+    return tokenString, nil
+}
+
+func ValidateToken(tokenString string) (*Claims, error) {
+    token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+        return jwtSecret, nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+        return claims, nil
+    }
+
+    return nil, errors.New("invalid token")
+}
